a4946c09bcff1aa162de1e13a66280bbfe452b52
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-13 21:27:30 -0600

    helper redux devtool added

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@ import { createStore } from 'redux';
 import rootReducer from './reducers/index'
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
